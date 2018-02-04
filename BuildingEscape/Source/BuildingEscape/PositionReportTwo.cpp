dfb02805bc66b9588fe655c81268e048b2f730d4
Anthony Gutierrez <adgutierrez@techbyte.net>
2018-02-04 14:08:14 -0800

    Section 3, Lecture 65

--- BuildingEscape/Source/BuildingEscape/PositionReportTwo.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReportTwo.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReportTwo.h"
-
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReportTwo::UPositionReportTwo()
@@ -19,7 +19,9 @@ void UPositionReportTwo::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position Report Two reporting!"));
+	FString ObjectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position Report Two reporting %s"), *ObjectName);
 	
 }
 
